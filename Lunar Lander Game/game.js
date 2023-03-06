6de5c61cbf5af2098025685f92c254d805720323
Ellinelliey <hali22ow@student.ju.se>
2023-03-06 23:44:00 +0100

    ah why so difficult

--- Lunar Lander Game/game.js
+++ Lunar Lander Game/game.js
@@ -7,11 +7,18 @@ function ufoShip(x, y) {
   fill(255, 255, 255);
   rect(x, y, 100, 100);
 }
-let x = 100;
-let y = 100;
+let ufoX = 100;
+let ufoY = 100;
+let thrustY = 2;
 function draw() {
   clear();
-  ufoShip(x, y);
+  ufoShip(ufoX, ufoY);
   //y = y + 5;
   //x = x + 5;
+}
+
+if (playingGame) {
+  if (keyIsPressed(32)) {
+    thrustY = -1;
+  }
 }

5a705707d606a1143dda56a0ab039e8ba382c86b
Ellinelliey <hali22ow@student.ju.se>
2023-03-07 15:33:49 +0100

    bluh

--- Lunar Lander Game/game.js
+++ Lunar Lander Game/game.js
@@ -9,8 +9,7 @@ function ufoShip(x, y) {
 }
 let ufoX = 100;
 let ufoY = 100;
-let speedY = 7;
-let speed = 1;
+let speedY = 0;
 let playingGame = true;
 let startScreen = false;
 let succedScreen = false;
@@ -19,16 +18,15 @@ let failedScreen = false;
 function draw() {
   clear();
   ufoShip(ufoX, ufoY);
-  ufoX = ufoX + thrustX;
-  ufoY = ufoY + thrustY;
 
-  let playingGame = true;
+  playingGame = true;
   if (playingGame) {
     if (keyIsDown(32)) {
-      speed = speed + 1;
+      speedY = speedY + 1;
     }
   }
 }
+function keyIsDown() {}
 
 if (isGameActive) {
   if (keyIsDown(38)) {

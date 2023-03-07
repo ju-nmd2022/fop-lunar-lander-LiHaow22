b52b8a4e3971ae780d0591a4f8a8696a71320ea1
Ellinelliey <hali22ow@student.ju.se>
2023-03-07 16:55:35 +0100

    crap

--- Lunar Lander Game/game.js
+++ Lunar Lander Game/game.js
@@ -1,36 +1,60 @@
+let ufoX = 100;
+let ufoY = 100;
+let speedY = 6;
+let yVal;
+let velocity;
+let accelerate;
+let mass;
+let playingGame = true;
+let startScreen = false;
+let succedScreen = false;
+let failedScreen = false;
+
 function setup() {
   createCanvas(800, 600);
   background(0);
+  yVal = 0;
+  velocity = 0;
+  mass = 100;
+  accelerate = mass * 0.01;
 }
 
 function ufoShip(x, y) {
   fill(255, 255, 255);
   rect(x, y, 100, 100);
 }
-let ufoX = 100;
-let ufoY = 100;
-let speedY = 0;
-let playingGame = true;
-let startScreen = false;
-let succedScreen = false;
-let failedScreen = false;
-
 function draw() {
   clear();
+  velocity += accelerate;
+  yVal += velocity;
+  ufoShip(width / 2, yVal, mass, mass);
+  if (yVal > height - mass / 2);
+}
+function mousePressed() {
+  yVal = 0;
+  velocity = 0;
+}
+function draw() {
+  playingGame = true;
+  if (playingGame) {
+    if (keyIsDown(32)) {
+      velocity = velocity - 1;
+    }
+  }
+}
+
+/*function draw() {
+  clear();
   ufoShip(ufoX, ufoY);
+  ufoY = ufoY + speedY;
 
   playingGame = true;
   if (playingGame) {
     if (keyIsDown(32)) {
-      speedY = speedY + 1;
+      speedY = speedY - 1;
     }
   }
 }
-function keyIsDown() {}
+*/
 
-if (isGameActive) {
-  if (keyIsDown(38)) {
-  } else {
-  }
-}
-console.log(velocity);
+console.log(speedY);

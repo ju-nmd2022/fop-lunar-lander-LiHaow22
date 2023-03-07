54ca35b9fd5a50910680809728699d9147bead53
Ellinelliey <hali22ow@student.ju.se>
2023-03-07 17:36:34 +0100

    copied over my ufo to game.js

--- Lunar Lander Game/game.js
+++ Lunar Lander Game/game.js
@@ -20,8 +20,47 @@ function setup() {
 }
 
 function ufoShip(x, y) {
+  //Landing gear (triangles)
+  //left
+  fill(217, 240, 255);
+  noStroke();
+  triangle(x - 50, y + 10, x - 80, y + 10, x - 75, y + 70);
+  //right
+  triangle(x + 50, y + 10, x + 80, y + 10, x + 75, y + 70);
+  //middle
+  triangle(x - 15, y + 10, x + 15, y + 10, x, y + 80);
+
+  //Landing gear (circles/balls)
+  //left
   fill(255, 255, 255);
-  rect(x, y, 100, 100);
+  ellipse(x - 75, y + 70, 10);
+  //right
+  ellipse(x + 75, y + 70, 10);
+  //middle
+  ellipse(x, y + 80, 10);
+
+  //Glass dome
+  fill(255, 255, 255);
+  stroke(217, 240, 255);
+  ellipse(x, y, 90);
+
+  //"metal" body/hull
+  fill(167, 187, 236);
+  stroke(255, 255, 255);
+  ellipse(x, y + 20, 200, 50);
+  ellipse(x, y + 15, 200, 50);
+  fill(255, 255, 255);
+  strokeWeight(2);
+  stroke(217, 240, 255);
+  ellipse(x, y, 110, 20);
+
+  //lines on top of hull
+  stroke(255, 255, 255);
+  line(x, y + 15, x, y + 30);
+  line(x - 40, y + 12, x - 60, y + 25);
+  line(x + 40, y + 12, x + 60, y + 25);
+  line(x - 60, y, x - 80, y + 2);
+  line(x + 60, y, x + 80, y + 2);
 }
 function draw() {
   clear();
